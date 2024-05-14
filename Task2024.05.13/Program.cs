f6449e682464283279d91dafcced7db35c22686a
guliyevashahjahan <shahjahaneg@code.edu.az>
2024-05-14 10:34:40 +0400

    Done

---
+++ Task2024.05.13/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace Task2024._05._13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 2; i <= number / 2; i++)
+            {
+                if (number % i == 0)
+                {
+                    Console.WriteLine(number + " is not a prime number");
+                    return;
+                }
+
+            }
+            Console.WriteLine(number + " is a prime number");
+           
+
+            /////////////////////////////////////////////////////
+
+            Console.WriteLine("Enter number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int counter = 0;
+            while (num > 0)
+            {
+                num /= 10;
+                counter++;
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
